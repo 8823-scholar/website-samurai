56d4d3474e435c00c32d209d295926c26cd85a94
KIUCHI Satoshinosuke <scholar@hayabusa-lab.jp>
2010-01-08 10:38:06 +0000

    BEFOOLのSamuraiライブラリを参照するようにしました
    git-svn-id: http://system.befool.co.jp/svn/samurai.web/trunk@112 86b6696d-43f4-47ef-843a-e2e12e358140

--- Samurai/www/info.php
+++ Samurai/www/info.php
@@ -1,23 +1,25 @@
-<?
+<?php
 /**
- * www/info.php
+ * Samurai::info()を出力するためのエントリーポイント
  * 
- * Samurai::info()を出力するためのエントリーポイント。
- * 
- * @package    Package
- * @subpackage Www
- * @copyright  Foo Project
- * @author     Foo Bar <foo@bar.jp>
+ * @package    Samurai
+ * @copyright  2007-2010 Samurai Framework Project
+ * @author     KIUCHI Satoshinosuke <scholar@hayabusa-lab.jp>
+ * @license    http://www.opensource.org/licenses/bsd-license.php The BSD License
  */
+
+require_once 'Samurai/Samurai.class.php';
+
 //SamuraiFWの起動
 define('SAMURAI_APPLICATION_NAME', 'web');
 if(!preg_match('/samurai-fw\.org$/', $_SERVER['HTTP_HOST'])){
     define('SAMURAI_ENVIRONMENT', 'local');
+} else {
+    Samurai::unshiftSamuraiDir('/usr/share/pear/Samurai.BEFOOL');
 }
-include_once('Samurai/Samurai.class.php');
 Samurai::unshiftSamuraiDir(dirname(dirname(__FILE__)));
 Samurai::init();
 Samurai_Config::set('action.default', 'samurai_info');
-//Samurai_Controllerの起動
 $Controller = Samurai::getContainer()->getComponent('Controller');
 $Controller->execute();
+
